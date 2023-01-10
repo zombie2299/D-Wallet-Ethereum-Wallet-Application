e1bc72eee0396fe7db800749d9761975c0418a00
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2023-01-10 10:48:21 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
+import Web3 from "web3";
 import Welcome from "./components/Welcome.jsx";
 import Accounts from "./components/Accounts.jsx";
 import SendEther from "./components/SendEther.jsx";
 import "./App.css";
 
-const App = () => {
+function App() {
+  const [web3, setWeb3] = useState(null);
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const web3 = new Web3("http://127.0.0.1:7545");
+        setWeb3(web3);
+        // console.log(state);
+      } catch (error) {
+        alert("Falied to load web3 or contract.");
+        console.log(error);
+      }
+    };
+    init();
+  }, []);
   return (
     <div className="Flex">
       <div className="welMargin">
